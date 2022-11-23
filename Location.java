08b315844083249106f59de9c6699623e9483fd9
Des'ree Luijt <476349jl@student.eur.nl>
2022-11-23 16:09:53 +0100

    ik heb location toegevoegd

---
+++ Location.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Location {
+	List<Double> demand;
+	double averageDemand;
+	double x;
+	double y;
+
+	public Location(List<Double> demand, double averageDemand, double x, double y) {
+		this.demand = demand;
+		this.averageDemand = averageDemand;
+		this.x = x;
+		this.y = y;
+	}
+}
